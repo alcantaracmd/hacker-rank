58c5cca20f924f54c7b2e893ecd3fcf673eec846
Alcantara <alcantaracmd@gmail.com>
2025-11-14 21:22:42 -0300

    add staircase

---
+++ staircase.js
@@ -0,0 +1,7 @@
+function staircase(n) {
+    for (let i = 1; i <= n; i++) {
+        const spaces = " ".repeat(n - i); 
+        const hashes = "#".repeat(i);
+        console.log(spaces + hashes);
+    }
+}
